0eeb1923cb43770345e5cf1232c923b4f752aeb0
rafaelxxz <r.figueira@escola.pr.gov.br>
2025-10-22 08:55:41 -0300

    drt

---
+++ app.js
@@ -0,0 +1,8 @@
+function criaCartao(categoria,pergunta,resposta){
+    let container=
+    document.grtElementByld('container')
+    let cartao=document.createElement('article')
+    cartao.className
+
+}
+
