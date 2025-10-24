5fbfbf59397fdccdfa655514d6e85535304eb3d5
rafaelxxz <r.figueira@escola.pr.gov.br>
2025-10-24 10:45:20 -0300

    yujfyh

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 function criaCartao(categoria,pergunta,resposta){
-    let container=
-    document.grtElementByld('container')
-    let cartao=document.createElement('article')
-    cartao.className
-
+    let container=document.getElementByld('container')
+    let cartao = document.createElement('article')
+    CredentialsContainer.className='cartao'
 }
+cartao.innerHtml= 
+<div class='cartao_conteudo'>
+    <h3>${categoria}</h3>
+    <div class="cartao_conteudo_pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class="cartao_conteudo_resposta">
+        <p>${resposta}</p>
+    </div>
+</div>
+
+    
+    
+    
+
+
 
---
+++ perguntas.js
@@ -0,0 +1,13 @@
+criaCartao(
+    'programação'
+    'O que é python?',
+     'O python é uma linguagem de programação'
+
+)
+criaCartao(
+'Geografia',
+'Qual a capital da frança?',
+)
+
+
+
